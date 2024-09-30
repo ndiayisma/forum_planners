2e70304295ab70d1fa02dc8f82849cd52a380a7f
ndiayisma <ismail.ndiaye@lycee-faure.fr>
2024-09-30 17:24:27 +0200

    Search Js

--- assets/search.js
+++ assets/search.js
@@ -1,1 +1,20 @@
 console.log("Asset Mapper c\'est trop bien ! ");
+
+//search user from input (id=search) and filter rows from table bod (id=userTable)
+
+document.getElementById('search').addEventListener('keyup', function() {
+    var input = document.getElementById('search').value;
+    var table = document.getElementById('userTable');
+    var tr = table.getElementsByTagName('tr');
+    for (var i = 0; i < tr.length; i++) {
+        var td = tr[i].getElementsByTagName('td')[0];
+        if (td) {
+            var textValue = td.textContent || td.innerText;
+            if (textValue.indexOf(input) > -1) {
+                tr[i].style.display = '';
+            } else {
+                tr[i].style.display = 'none';
+            }
+        }
+    }
+}
